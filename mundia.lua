2ba696b792f3332c092d7353485d61a4f969ea7a
Arkiver2 <Arkiver@hotmail.com>
2014-08-28 21:41:36 +0200

    mundia.lua: add scrape for search results urls
    Example: http://www.mundia.com/pk/Search/Results?surname=ABDULA&birthPlace=Verenigde%20Staten

--- mundia.lua
+++ mundia.lua
@@ -28,19 +28,51 @@ end
 wget.callbacks.get_urls = function(file, url, is_css, iri)
   local urls = {}
   local html = nil
-  
+  local mundia_url = "http://www.mundia.com"
   --example url: http://www.mundia.com/us/Person/743375/6809259973
   --example url: http://www.mundia.com/us/Person/12748608/-190814136
-  if string.match(url, "%.mundia%.com/[a-z]+/Person/[0-9]+/[0-9-]+") then
+  
+  
+  --example url: http://www.mundia.com/pk/Search/Results?surname=ABDULA&birthPlace=Verenigde%20Staten
+  if string.match(url, "%.mundia%.com/[a-z]+/Search/Results?surname=[A-Z]+%&birthPlace=[.]+") then
+    if not html then
+      html = read_file(file)
+    end
     
-  elseif string.match(url, "%.mundia%.com/[a-z]+/Person/[0-9]+/[0-9]+") then
+    --example string: <a href="/pk/Person/5586782/-1432906874" class="">Joseph Sadula Abdula</a>
+    for person_url in string.gmatch(html, '<a href="(/[a-z]+/Person/[0-9]/[-]?[0-9]+)" class="">[a-z]+</a>') do
+      --------------Multiple links possible as results probably - chfoo - help?-------------------
+      table.insert(urls, { url=mundia_url..person_url })
+    end
+    
+    --example string: <a class="tree" href="/pk/Tree/Family/5586782/-1432906874"><span class="view-tree">Stamboom tonen</span></a>
+    for tree_url in string.gmatch(html, '<a class="[a-z]+" href="(/[a-z]+/Tree/Family/[0-9]/[-]?[0-9]+)"><span class="view-tree">[a-z]+</span></a>') do
+      --------------Multiple links possible as results probably - chfoo - help?-------------------
+      table.insert(urls, { url=mundia_url..tree_url })
+    end
+    
+    --example string: <img src="http://mediasvc.ancestry.com/v2/image/namespaces/1093/media/11f96e77-c39c-4ca4-b659-32f67aa8d129.jpg?client=TreeService&MaxSide=96" width="68" alt="Foto" /></a>
+    for person_image in string.gmatch(html, '<img src="(http://mediasvc%.ancestry%.com/v[0-9]+/image/namespaces/[0-9]+/media/[a-z0-9-]+%.jpg%?client=TreeService&MaxSide=[0-9]+)" width="[0-9]+" alt="Foto" /></a>') do
+      --------------Multiple links possible as results probably - chfoo - help?-------------------
+      table.insert(urls, { url=person_image })
+      for person_image_big in string.gmatch(person_image, "(http://mediasvc%.ancestry%.com/v[0-9]+/image/namespaces/[0-9]+/media/[a-z0-9-]+%.jpg%?client=TreeService)&MaxSide=[0-9]+") do
+        table.insert(urls, { url=person_image_big })
+      end
+    end
+  end
+  
+  --example url: http://www.mundia.com/pk/Person/5586782/-1432906874
+  if string.match(url, "%.mundia%.com/[a-z]+/Person/[0-9]+/[-]?[0-9]+") then
+    if not html then
+      html = read_file(file)
+    end
+    
+    --example string: href="/pk/Messages?sendMessageTo=0120cac9-0003-0000-0000-000000000000&subject=Joseph%2BSadula%2BAbdula"
+    for adding_user in string.gmatch(html, 'href="(/[a-z]+/Messages%?sendMessageTo=[0-9-]+&subject=[a-z%]+)"') do
+    end
     
   end
   
-  --example url: http://www.mundia.com/us/Search/Results?surname=AMES&birthPlace=United%20States
-  if string.match(url, "%.mundia%.com/[a-z]+/Search/Results%?surname=[A-Z]+&birthPlace=[a-zA-z%20]+") then
-    
-  end
 end
   
 
